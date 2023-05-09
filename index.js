e73357fb4a364b3f68b8cfd7ce19918f0921cdf9
Guilherme Mazzolini <guilhermemazzolini@hotmail.com>
2023-05-09 16:33:40 +0200

    my Portfolio

---
+++ index.js
@@ -0,0 +1,47 @@
+import portfolio from "./data.json" assert {type: "json"}
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+const imgPortfolio = document.getElementById('portfolio');
+const portfolioPage = document.getElementById('portfoliopage')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
+
+for (let i = 0; i < portfolio.length; i++) {
+    const img = document.createElement("img");
+    img.src = portfolio[i].image;
+    img.classList.add("portfolio__img");
+    img.onclick = function() {
+        window.open(`portfolio-item.html?index=${i}`, "_self");
+
+  };
+  imgPortfolio?.appendChild(img);
+};
+
+function displayItemInfo() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const index = parseInt(urlParams.get('index'));
+    const item = portfolio[index];
+
+    portfolioPage.innerHTML = `
+    <section class="intro">
+        <h1 class="section__title section__title--intro">${item.title}</h1>
+         <a href=${item.gitHubLink} target="_blank" class="section__subtitle section__subtitle--intro" id="git--button">GitHub Link</a>
+         <img src="${item.image}" alt="" class="intro__img">
+     </section>
+     <div class="portfolio-item-individual">
+         <p>${item.shortDescription}</p>
+         <img src="${item.image}" alt="">
+         <p>${item.fullDescription}</p>
+     </div>
+    `
+  };
+
+displayItemInfo();
